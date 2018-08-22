e90317c33419754413b4d5cb04d204756adbc355
Arseny Egorov <arsegorov@gmail.com>
2018-08-22 14:57:25 -0400

    Added the function signature for SET

---
+++ src/set.py
@@ -0,0 +1,11 @@
+def set_psql(tsql):
+    """
+    Translates the `SET` statement from T-SQL to PostgresQL.
+
+    :param tsql: The `SET` statement in T-SQL (i.e., SQL Server) syntax
+    :type tsql: string
+    :return: The equivalent statement in PostgresQL syntax if one exists
+    :rtype: string
+    """
+
+    pass
